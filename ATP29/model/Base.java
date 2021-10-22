5a69bc25d6c534d40c42934ec348c63e438a525b
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-10-22 11:43:52 -0300

    View Método Main

--- ATP29/model/Base.java
+++ ATP29/model/Base.java
@@ -1,5 +1,22 @@
 package ATP29.model;
 
 public class Base {
-    public int id;  
+    public String id;  
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Base) {
+            Base outro = (Base) obj;
+            if (this.id.equals(outro.id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String retorno = this.id;
+        return retorno;
+    }
 }
--- ATP29/model/Produto.java
+++ ATP29/model/Produto.java
@@ -3,5 +3,21 @@ package ATP29.model;
 public class Produto extends Base {
     public Categoria categoria;
     public String dvd;
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj instanceof Produto){
+            Produto outro = (Produto)obj;
+            if (this.id.equals(outro.id) && this.dvd.equals(outro.dvd) && this.categoria.equals(outro.categoria)){
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public String toString(){
+        String retorno = this.dvd;
+        return retorno;
+    }
 }
 
